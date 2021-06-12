08256828758be5ebdcf64481dbd59705ccda01a7
nathan hishon <nathan.hishon@gmail.com>
2021-06-12 17:11:43 -0500

    rebuild

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", require("./routes/jwtAuth"))
-app.use("/dashboard", require("./routes/dashboard"))
+app.use("/dashboard", require("./routes/dashboard"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build/index.html"));
